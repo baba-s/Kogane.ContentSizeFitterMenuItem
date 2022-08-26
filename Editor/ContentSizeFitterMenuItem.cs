2b0fa5d5cb35c18363a76586f43b7dd1716b448a
baba-s <shotababa1206@gmail.com>
2022-08-26 23:38:27 +0900

    add

---
+++ Editor/ContentSizeFitterMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine.UI;
+
+namespace Kogane.Internal
+{
+    internal static class ContentSizeFitterMenuItem
+    {
+        [MenuItem( "CONTEXT/ContentSizeFitter/Set Preferred Size" )]
+        private static void SetPreferredSize( MenuCommand menuCommand )
+        {
+            const ContentSizeFitter.FitMode fitMode = ContentSizeFitter.FitMode.PreferredSize;
+
+            var fitter = ( ContentSizeFitter )menuCommand.context;
+
+            Undo.RecordObject( fitter, "Set Preferred Size" );
+
+            fitter.horizontalFit = fitMode;
+            fitter.verticalFit   = fitMode;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.ContentSizeFitterMenuItem
+# Kogane Content Size Fitter Menu Item
+
+ContentSizeFitter の FitMode をすべて「PreferredSize」にする MenuItem を追加するエディタ拡張
